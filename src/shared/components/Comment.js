732e83b6faa9e71a4c467ba063f76ee9baab8f36
Konrad Urbańczyk <elearning.warsaw@gmail.com>
2022-02-13 11:51:33 +0100

    fixed comments section

---
+++ src/shared/components/Comment.js
@@ -0,0 +1,49 @@
+import React from "react";
+import formatDate from "../utils/formatDate";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth-context";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Comment = ({ comment }) => {
+  const { userId, token } = useContext(AuthContext);
+  const isAuthor = comment.author?.id === userId;
+
+  const deleteHandler = () => {};
+
+  return (
+    <Wrapper>
+      <div className="me-3">
+        <p className="mb-3">
+          <Link className="card-link" to={`/users/${comment.author?.id}`}>
+            {comment.author?.username}{" "}
+          </Link>
+          <span className="ms-2 text-muted">{formatDate(comment.posted)}</span>
+        </p>
+        <p>{comment.content}</p>
+      </div>
+
+      {isAuthor && (
+        <div className="d-flex justify-content-end h-25" >
+          <button className="btn btn-sm btn-outline-danger">
+            <i style={{ fontSize: "12px" }} className="bi bi-trash"></i>
+          </button>
+        </div>
+      )}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  font-size: 14px;
+  border-bottom: 1px solid gainsboro;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 2rem 0 0.5rem 0;
+
+
+
+`;
+
+export default Comment;
--- src/shared/components/Comments.js
+++ src/shared/components/Comments.js
@@ -1,13 +1,23 @@
-import React, { useEffect, useState, useContext } from "react";
-import formatDate from "../utils/formatDate";
+import React, { useEffect, useState, useContext, useCallback } from "react";
 import useHttp from "../hooks/http-hook";
 import { AuthContext } from "../../shared/context/auth-context";
+import Comment from "./Comment";
+import TextareaAutosize from "react-textarea-autosize";
 
 const Comments = ({ itemId, itemType }) => {
   const { fetchData, sendItem } = useHttp();
   const [commentList, setCommentList] = useState([]);
   const [newComment, setNewComment] = useState("");
   const { userId, token } = useContext(AuthContext);
+
+  const fetchComments = useCallback(async () => {
+    try {
+      const response = await fetchData(`comments/${itemId}?type=${itemType}`);
+      const { comments } = response;
+
+      setCommentList(comments);
+    } catch (err) {}
+  }, [itemId, itemType]);
 
   const submitHandler = async function (event) {
     event.preventDefault();
@@ -26,53 +36,48 @@ const Comments = ({ itemId, itemType }) => {
           Authorization: "Bearer " + token,
         },
       });
+
+      fetchComments();
+      setNewComment("");
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const response = await fetchData(`comments/${itemId}?type=${itemType}`);
-        const { comments } = response;
-
-        setCommentList(comments);
-      } catch (err) {}
-    };
     try {
       fetchComments();
     } catch (err) {}
-  }, [itemId]);
+  }, [fetchComments]);
 
   return (
     <div>
-      <form onSubmit={submitHandler}>
-        <textarea
-          className="form-control"
-          name="content"
-          placeholder="Write comment..."
-          onChange={(e) => {
-            setNewComment(e.target.value);
-          }}
-          value={newComment}
-        ></textarea>
-        {!!newComment && (
-          <button className="btn btn-success" type="submit">
-            Send
-          </button>
-        )}
-      </form>
+      {!!token ? (
+        <form onSubmit={submitHandler}>
+          <TextareaAutosize
+            className="form-control"
+            name="content"
+            placeholder="Write a comment..."
+            minRows={2}
+            maxRows={10}
+            onChange={(e) => {
+              setNewComment(e.target.value);
+            }}
+            value={newComment}
+          ></TextareaAutosize>
+          {!!newComment && (
+            <div className="d-flex justify-content-end mt-2">
+              <button className="btn btn-success" type="submit">
+                Send
+              </button>
+            </div>
+          )}
+        </form>
+      ) : (
+        <p className="lead">Log in to write a comment.</p>
+      )}
       {commentList.map((item) => {
-        return (
-          <div>
-            <h6>
-              {item.author?.username}{" "}
-              <span className="ms-2 text-muted">{formatDate(item.posted)}</span>
-            </h6>
-            <p>{item.content}</p>
-          </div>
-        )
+        return <Comment comment={item} />;
       })}
     </div>
   );
